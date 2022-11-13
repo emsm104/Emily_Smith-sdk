9883b50250c155f7bee4bf1d13a9fb05b35ac1ac
Emily Smith <emilysmith@Emilys-MacBook-Pro.local>
2022-11-13 01:45:23 -0600

    remove token

--- lotr_sdk/settings.py
+++ lotr_sdk/settings.py
@@ -1,5 +1,5 @@
 # API Version:
 # Please check here for version updates: https://the-one-api.dev/
 API = 'https://the-one-api.dev/v2/'
-TOKEN = 'hsvIgn1LIo2FihfLRjP9'
+TOKEN = ''
 AUTH_HEADER = {"Authorization": "Bearer " + TOKEN}
